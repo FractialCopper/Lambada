963ea079c100d7545e4ecc495165078fc8f0149e
heyimjustalex <heyimjustalex@gmail.com>
2020-06-29 21:48:30 +0200

    Initial configuration OpenGl GLFW imGui and gTest

---
+++ gTest/gTest.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+#include <imgui/imgui.h>
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
---
+++ opengl_IMGUI/README.txt
@@ -0,0 +1,3 @@
+Jeœli nie dzia³a:
+
+PPM na solution -> properties -> general -> windows sdk/platform toolset zmienic na zainstalowane
